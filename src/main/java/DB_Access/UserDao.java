d5a753842ce5cfc169b79ed2bb88a44a5245bd9b
brightlightkim <taeyangk95@naver.com>
2021-10-08 08:58:29 +0900

    built up userDao class

--- src/main/java/DB_Access/UserDao.java
+++ src/main/java/DB_Access/UserDao.java
@@ -1,4 +1,19 @@
 package DB_Access;
 
+import DB.*;
+
+import java.util.List;
+
 public class UserDao {
+    void createUser(User user){}
+    boolean validate(String username, String password){
+        return false;
+    }
+    User getUserByID(String userId){
+        return null;
+    }
+    List<Event> findUserEvents(String username){
+        return null;
+    }
+
 }
