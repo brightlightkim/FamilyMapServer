4ed116bf5968957d256dab218c384e2e4292f978
brightlightkim <taeyangk95@naver.com>
2021-10-08 08:51:49 +0900

    built up the authtoken class

---
+++ src/main/java/DB/authtoken.java
@@ -0,0 +1,27 @@
+package DB;
+
+public class authtoken {
+    private String authToken;
+    private String username;
+
+    public authtoken(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
