6cb569c7293dffce8d425eef55815f19bc3ff317
brightlightkim <taeyangk95@naver.com>
2021-10-08 11:50:27 +0900

    add all javadoc for service classes

--- src/main/java/Service/PersonService.java
+++ src/main/java/Service/PersonService.java
@@ -3,7 +3,20 @@ package Service;
 import Result.AllPersonResult;
 import Result.PersonResult;
 
+/**
+ * Class that performs person relating methods
+ * It can either find one person or every person
+ */
 public class PersonService {
+    /**
+     * Method that shows the result of finding one person
+     * @return one person
+     */
     public PersonResult personResult(){return null;}
+
+    /**
+     * Method that shows the result of finding every person
+     * @return every person
+     */
     public AllPersonResult allPersonResult(){return null;}
 }
