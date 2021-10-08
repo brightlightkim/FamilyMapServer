b6b40c9d501d3ad7127935c33ece95d9d61d79b7
brightlightkim <taeyangk95@naver.com>
2021-10-08 10:01:03 +0900

    changed details

--- src/main/java/Service/EventService.java
+++ src/main/java/Service/EventService.java
@@ -1,14 +1,10 @@
 package Service;
 
-import Model.Event;
-import Request.EventRequest;
 import Result.EventResult;
 
 public class EventService {
     //take a request and return the response.
     //take a request object and return the return object.
-
     //requires only 1 method
-    public EventResult requestEvent(EventRequest request){return null;}
-
+    public EventResult requestEvent(){return null;}
 }
