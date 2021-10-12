a3ca4bbce144e964456f1da5eac95fcf4ede7019
brightlightkim <taeyangk95@naver.com>
2021-10-12 20:49:56 +0900

    added some codes for insert, find, and clear methods of UserDAO.java

--- src/FMS/DataAccess/UserDAO.java
+++ src/FMS/DataAccess/UserDAO.java
@@ -4,6 +4,7 @@ import Model.*;
 import Exception.DataAccessException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -40,15 +41,52 @@ public class UserDAO {
     }
 
     /**
-     * find the user by id
-     * @param id id of the user
+     * find the user by username
+     * @param username username of the user
      * @return user
      */
-    public User find(String id){return null;}
+    public User find(String username) throws DataAccessException {
+        User user;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM Users WHERE Username = ?;";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                user = new User(rs.getString("Username"), rs.getString("Password"),
+                        rs.getString("Email"), rs.getString("FirstName"),
+                        rs.getString("LastName"), rs.getString("Gender"),
+                        rs.getString("PersonID"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DataAccessException("Error encountered while finding event");
+        } finally {
+            if(rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return null;
+    }
 
     /**
      * clear users
      */
-    public void clear(){}
+    public void clear() throws DataAccessException{
+        String sql = "DELETE FROM ?;";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, "Users");
+            stmt.executeQuery();
+        } catch (SQLException e){
+            e.printStackTrace();
+            throw new DataAccessException("There is no database at all!");
+        }
+    }
 
 }
