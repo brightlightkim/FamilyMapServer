ac6b7bb7e39319b035ae94072c4a94356c5dcca1
brightlightkim <taeyangk95@naver.com>
2021-10-12 15:53:05 +0900

    add PracticeJUnit.java PracticeAssertionsJUnit.java

--- src/test/java/passoff/PracticeJUnit.java
+++ src/test/java/passoff/PracticeJUnit.java
@@ -10,4 +10,40 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class PracticeJUnit {
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    @Test
+    void succeedingTest() {
+    }
+
+    @Test
+    void failingTest() {
+        fail("a failing test");
+    }
+
+    @Test
+    @Disabled("for demonstration purposes")
+    void skippedTest() {
+        // not executed
+    }
+
+    @Test
+    void abortedTest() {
+        assumeTrue("abc".contains("Z"));
+        fail("test should have been aborted");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
 }
