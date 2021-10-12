528d44acde840ba06bf5e84abd323d6f72885ea6
brightlightkim <taeyangk95@naver.com>
2021-10-12 15:47:21 +0900

    add PracticeJUnit.java

---
+++ src/test/java/passoff/PracticeJUnit.java
@@ -0,0 +1,13 @@
+package passoff;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class PracticeJUnit {
+}
