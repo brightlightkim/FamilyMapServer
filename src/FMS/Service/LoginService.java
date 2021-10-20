c13e792f31f1921388ce6db463854bfee2f258f2
brightlightkim <taeyangk95@naver.com>
2021-10-20 20:08:42 +0900

    made a basic LoginService.java

--- src/FMS/Service/LoginService.java
+++ src/FMS/Service/LoginService.java
@@ -16,8 +16,29 @@ public class LoginService {
      */
     public LoginResult login(LoginRequest request) throws DataAccessException {
         Database db = new Database();
-        db.openConnection();
+        try{
+            db.openConnection();
+            // Use DAOs to do requested operation
+            new DaoA(db.getConnection()).dbOpA(request.x, request.y);
+            new DaoB(db.getConnection()).dbOpB(request.z);
 
+            // Close database connection, COMMIT transaction
+            db.closeConnection(true);
+
+            // Create and return SUCCESS Result object
+            Result result = new Result(true, a, b, c);
+            return result;
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+
+            // Close database connection, ROLLBACK transaction
+            db.closeConnection(false);
+
+            // Create and return FAILURE Result object
+            Result result = new Result(false, “Error message”);
+            return result;
+        }
 
         return null;
     }
