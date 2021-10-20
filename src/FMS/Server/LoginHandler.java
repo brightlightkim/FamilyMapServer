d5be476ccaf7cef833234e061ddff1706c7cbba9
brightlightkim <taeyangk95@naver.com>
2021-10-20 20:57:24 +0900

    succinct coding

--- src/FMS/Server/LoginHandler.java
+++ src/FMS/Server/LoginHandler.java
@@ -6,16 +6,14 @@ import Result.LoginResult;
 import Service.LoginService;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.util.Locale;
 
-public class LoginHandler implements HttpHandler {
+public class LoginHandler extends Handler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        boolean success = false;
         Gson gson = new Gson();
         try {
             if (exchange.getRequestMethod().toLowerCase(Locale.ROOT).equals("post")){
@@ -40,13 +38,4 @@ public class LoginHandler implements HttpHandler {
             e.printStackTrace();
         }
     }
-    /*
-        The writeString method shows how to write a String to an OutputStream.
-    */
-    private void writeString(String str, OutputStream os) throws IOException {
-        OutputStreamWriter sw = new OutputStreamWriter(os);
-        sw.write(str);
-        sw.flush();
-    }
-
 }
