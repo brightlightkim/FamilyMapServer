61f66c4f4b920a809f4d91274108aad257dd3dc8
brightlightkim <taeyangk95@naver.com>
2021-10-25 17:59:00 +0900

    make a plan for the coding

--- src/FMS/Service/RegisterService.java
+++ src/FMS/Service/RegisterService.java
@@ -34,13 +34,16 @@ public class RegisterService {
                 db.closeConnection(false);
                 return result;
             }
+            //Where recursion needs to occur..
+
             String uuid = UUID.randomUUID().toString();
 
-            //Create each objects for this person.
+            //Create User for this request
             User newUser = new User(request.getUsername(), request.getPassword(),
                     request.getEmail(), request.getFirstName(), request.getLastName(),
                     request.getGender(), uuid);
-            //TODO: Have to fix the part of father, mother, and spouse later.
+
+            //Create Person for this request
             String fatherID = UUID.randomUUID().toString();
             String motherID = UUID.randomUUID().toString();
             String spouseID = UUID.randomUUID().toString();
@@ -48,9 +51,12 @@ public class RegisterService {
                     request.getLastName(), request.getGender(), fatherID, motherID, spouseID);
 
 
-
+            //Create Token for this request
             uuid = UUID.randomUUID().toString(); //grant the random token for the username.
             AuthToken newToken = new AuthToken(uuid, request.getUsername());
+
+            //Create Event for this request >> Birth Event
+
 
             //Add it to the database
             new UserDAO(db.getConnection()).insert(newUser);
