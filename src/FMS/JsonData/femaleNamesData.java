d673e6c288309ebcba4182ddceb2f6af0b4ec76e
brightlightkim <taeyangk95@naver.com>
2021-10-25 18:17:42 +0900

    create more JSON data files

---
+++ src/FMS/JsonData/femaleNamesData.java
@@ -0,0 +1,17 @@
+package JsonData;
+
+public class femaleNamesData {
+    String[] data;
+
+    public femaleNamesData(String[] data) {
+        this.data = data;
+    }
+
+    public String[] getData() {
+        return data;
+    }
+
+    public void setData(String[] data) {
+        this.data = data;
+    }
+}
---
+++ src/FMS/JsonData/surnamesData.java
@@ -0,0 +1,17 @@
+package JsonData;
+
+public class surnamesData {
+    String[] data;
+
+    public surnamesData(String[] data) {
+        this.data = data;
+    }
+
+    public String[] getData() {
+        return data;
+    }
+
+    public void setData(String[] data) {
+        this.data = data;
+    }
+}
